5549ba537a20b3b7775ae10dd2892a8a717adc0c
mbabker <mbabker@90f216a6-2ee8-4e43-a5e7-8df160250c90>
2011-01-04 03:06:30 +0000

    Initial refactor of module for 1.6
    git-svn-id: http://subversion.assembla.com/svn/podcast-manager@5 90f216a6-2ee8-4e43-a5e7-8df160250c90

--- trunk/mod_podcastmanager/tmpl/default.php
+++ trunk/mod_podcastmanager/tmpl/default.php
@@ -1,4 +1,17 @@
-<?php defined('_JEXEC') or die('Restricted access'); ?>
+<?php
+/**
+* Podcast Manager for Joomla!
+*
+* @version		$Id$
+* @copyright	Copyright (C) 2011 Michael Babker. All rights reserved.
+* @license		GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+* 
+*/
+
+// Restricted access
+defined('_JEXEC') or die();
+?>
+
 <div><?php echo $params->get('text'); ?></div>
 <div><a href="<?php echo $link; ?>"><?php echo $img; ?></a></div>
-<?php if($params->get('plainlink')) { ?><div><a href="<?php echo $plainlink; ?>">Full Feed</a></div><?php } ?>
+<div><a href="<?php echo $plainlink; ?>"><?php echo JText::_('MOD_PODCASTMANAGER_FULLFEED');?></a></div>
