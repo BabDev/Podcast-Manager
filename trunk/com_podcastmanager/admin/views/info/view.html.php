7f6c43af7491d0c2a27132c1ce4bcd6d91a1050a
mbabker <mbabker@90f216a6-2ee8-4e43-a5e7-8df160250c90>
2011-01-05 17:24:41 +0000

    svn:keywords property
    git-svn-id: http://subversion.assembla.com/svn/podcast-manager@9 90f216a6-2ee8-4e43-a5e7-8df160250c90

--- trunk/com_podcastmanager/admin/views/info/view.html.php
+++ trunk/com_podcastmanager/admin/views/info/view.html.php
@@ -2,7 +2,7 @@
 /**
 * Podcast Manager for Joomla!
 *
-* @version		$Id: podcastmanager.php 7 2011-01-05 16:46:53Z mbabker $
+* @version		$Id$
 * @copyright	Copyright (C) 2011 Michael Babker. All rights reserved.
 * @license		GNU/GPL - http://www.gnu.org/copyleft/gpl.html
 * 
