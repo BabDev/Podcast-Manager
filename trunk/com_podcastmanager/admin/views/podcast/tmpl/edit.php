0f78f686e8299603094bc722ecc832c2e2c1aee6
mbabker <mbabker@90f216a6-2ee8-4e43-a5e7-8df160250c90>
2011-01-07 17:55:34 +0000

    Add edit view
    git-svn-id: http://subversion.assembla.com/svn/podcast-manager@35 90f216a6-2ee8-4e43-a5e7-8df160250c90

---
+++ trunk/com_podcastmanager/admin/views/podcast/tmpl/edit.php
@@ -0,0 +1,58 @@
+<?php 
+/**
+* Podcast Manager for Joomla!
+*
+* @version		$Id: default.php 9 2011-01-05 17:24:41Z mbabker $
+* @copyright	Copyright (C) 2011 Michael Babker. All rights reserved.
+* @license		GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+* 
+*/
+
+// Restricted access
+defined('_JEXEC') or die();
+
+$document =& JFactory::getDocument();
+$document->addScript(JURI::base() . 'components/com_podcastmanager/views/podcast/tmpl/default.js');
+
+?>
+<form action="<?php echo JRoute::_('index.php?option=com_podcastmanager&layout=edit&id='.(int) $this->item->podcast_id); ?>" method="post" name="adminForm" id="item-form" class="form-validate">
+	<div class="width-60 fltlft">
+		<fieldset class="adminform">
+			<legend><?php echo empty($this->item->id) ? JText::_('COM_PODCASTMANAGER_NEW_PODCAST') : JText::sprintf('COM_PODCASTMANAGER_EDIT_PODCAST', $this->item->podcast_id); ?></legend>
+			<ul class="adminformlist">
+				<li><?php echo $this->form->getLabel('filename'); ?>
+				<?php echo $this->form->getInput('filename'); ?></li>
+
+				<li><?php echo $this->form->getLabel('itAuthor'); ?>
+				<?php echo $this->form->getInput('itAuthor'); ?></li>
+
+				<li><?php echo $this->form->getLabel('itBlock'); ?>
+				<?php echo $this->form->getInput('itBlock'); ?></li>
+
+				<li><?php echo $this->form->getLabel('itCategory'); ?>
+				<?php echo $this->form->getInput('itCategory'); ?></li>
+
+				<li><?php echo $this->form->getLabel('itDuration'); ?>
+				<?php echo $this->form->getInput('itDuration'); ?></li>
+
+				<li><?php echo $this->form->getLabel('itExplicit'); ?>
+				<?php echo $this->form->getInput('itExplicit'); ?></li>
+
+				<li><?php echo $this->form->getLabel('itKeywords'); ?>
+				<?php echo $this->form->getInput('itKeywords'); ?></li>
+
+				<li><?php echo $this->form->getLabel('itSubtitle'); ?>
+				<?php echo $this->form->getInput('itSubtitle'); ?></li>
+
+				<li><?php echo $this->form->getLabel('language'); ?>
+				<?php echo $this->form->getInput('language'); ?></li>
+			</ul>
+		</fieldset>
+	</div>
+
+	<div>
+		<input type="hidden" name="task" value="" />
+		<input type="hidden" name="return" value="<?php echo JRequest::getCmd('return');?>" />
+		<?php echo JHtml::_('form.token'); ?>
+	</div>
+</form>
