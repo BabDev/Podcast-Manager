11939644415e40ed2ebc6fc825083325488434af
Michael Babker <mbabker@flbab.com>
2011-04-30 21:47:40 +0430

    Update overrides

--- files_podcastmanager_minima/com_podcastmedia/media/default_navigation.php
+++ files_podcastmanager_minima/com_podcastmedia/media/default_navigation.php
@@ -16,6 +16,7 @@ $style = $app->getUserStateFromRequest('podcastmedia.list.layout', 'layout', 'th
 		<div class="submenu-box">
 			<div class="submenu-pad">
 				<ul id="submenu" class="media">
+					<li><a href="index.php?option=com_podcastmanager&amp;view=feeds"><?php echo JText::_('COM_PODCASTMEDIA_SUBMENU_FEEDS'); ?></a></li>
 					<li><a href="index.php?option=com_podcastmanager&amp;view=podcasts"><?php echo JText::_('COM_PODCASTMEDIA_SUBMENU_PODCASTS'); ?></a></li>
 					<li><a href="index.php?option=com_podcastmanager&amp;view=info"><?php echo JText::_('COM_PODCASTMEDIA_SUBMENU_INFO'); ?></a></li>
 					<li><a id="thumbs" onclick="PodcastMediaManager.setViewType('thumbs')" class="<?php echo ($style == "thumbs") ? 'active' : '';?>">
