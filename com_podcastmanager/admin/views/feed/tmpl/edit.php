62f6dd51d8c3163214eb2e5295e6db3ac0ee9793
Michael Babker <mbabker@flbab.com>
2011-05-01 17:15:52 +0300

    Modify form

--- com_podcastmanager/admin/views/feed/tmpl/edit.php
+++ com_podcastmanager/admin/views/feed/tmpl/edit.php
@@ -25,26 +25,52 @@ JHtml::_('behavior.formvalidation');
 </script>
 
 <form action="<?php echo JRoute::_('index.php?option=com_podcastmanager&view=feed&layout=edit&id='.(int) $this->item->id); ?>" method="post" name="adminForm" id="item-form">
-<div class="width-60 fltlft">
-	<fieldset class="adminform">
-		<legend><?php echo JText::_('COM_PODCASTMANAGER_VIEW_FEED_FIELDSET_FEED');?></legend>
-			<ul class="adminformlist">
-				<li><?php echo $this->form->getLabel('title'); ?>
-				<?php echo $this->form->getInput('title'); ?></li>
+	<div class="width-60 fltlft">
+		<fieldset class="adminform">
+			<legend><?php echo JText::_('COM_PODCASTMANAGER_VIEW_FEED_FIELDSET_FEED');?></legend>
+				<ul class="adminformlist">
+					<li><?php echo $this->form->getLabel('name'); ?>
+					<?php echo $this->form->getInput('name'); ?></li>
 
-				<li><?php echo $this->form->getLabel('feedname'); ?>
-				<?php echo $this->form->getInput('feedname'); ?></li>
+					<li><?php echo $this->form->getLabel('published'); ?>
+					<?php echo $this->form->getInput('published'); ?></li>
 
-				<li><?php echo $this->form->getLabel('language'); ?>
-				<?php echo $this->form->getInput('language'); ?></li>
+					<li><?php echo $this->form->getLabel('language'); ?>
+					<?php echo $this->form->getInput('language'); ?></li>
 
-				<li><?php echo $this->form->getLabel('id'); ?>
-				<?php echo $this->form->getInput('id'); ?></li>
-			</ul>
-	</fieldset>
+					<li><?php echo $this->form->getLabel('id'); ?>
+					<?php echo $this->form->getInput('id'); ?></li>
+				</ul>
+		</fieldset>
+	</div>
 
-	<input type="hidden" name="task" value="" />
-	<?php echo JHtml::_('form.token'); ?>
+	<div class="width-40 fltrt">
+		<?php echo JHtml::_('sliders.start','podcastmanager-sliders-'.$this->item->id, array('useCookie'=>1)); ?>
+
+			<?php echo JHtml::_('sliders.panel',JText::_('JGLOBAL_FIELDSET_PUBLISHING'), 'publishing-details'); ?>
+			<fieldset class="panelform">
+				<ul class="adminformlist">
+					<li><?php echo $this->form->getLabel('created'); ?>
+					<?php echo $this->form->getInput('created'); ?></li>
+
+					<li><?php echo $this->form->getLabel('publish_up'); ?>
+					<?php echo $this->form->getInput('publish_up'); ?></li>
+
+					<?php if ($this->item->modified_by) : ?>
+					<li><?php echo $this->form->getLabel('modified_by'); ?>
+					<?php echo $this->form->getInput('modified_by'); ?></li>
+
+					<li><?php echo $this->form->getLabel('modified'); ?>
+					<?php echo $this->form->getInput('modified'); ?></li>
+					<?php endif; ?>
+				</ul>
+			</fieldset>
+		<?php echo JHtml::_('sliders.end'); ?>
+	</div>
+
+	<div>
+		<input type="hidden" name="task" value="" />
+		<?php echo JHtml::_('form.token'); ?>
 	</div>
 </form>
 <div class="clr"></div>
