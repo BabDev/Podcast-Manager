9768bc87b8aea16e5b53c934acec6cfe34f03b22
Michael Babker <mbabker@flbab.com>
2011-05-03 04:40:22 +0200

    Resync update SQL

--- com_podcastmanager/admin/sql/updates/mysql/podman_17.sql
+++ com_podcastmanager/admin/sql/updates/mysql/podman_17.sql
@@ -5,6 +5,7 @@ ALTER TABLE `#__podcastmanager` DEFAULT CHARSET=utf8;
 CREATE TABLE IF NOT EXISTS `#__podcastmanager_feeds` (
   `id` int(11) NOT NULL auto_increment,
   `name` varchar(255) default NULL,
+  `subtitle` varchar(255) NOT NULL default '',
   `description` varchar(255) NOT NULL default '',
   `copyright` varchar(255) NOT NULL default '',
   `explicit` tinyint(1) NOT NULL default '0',
