1bde2dc5d015bcfd1992260dcc043d941b5935d9
Michael Babker <mbabker@flbab.com>
2011-06-03 01:43:55 +0430

    Add JEXEC checks

--- com_podcastmedia/admin/helpers/podcastmedia.php
+++ com_podcastmedia/admin/helpers/podcastmedia.php
@@ -4,10 +4,13 @@
 *
 * @copyright	Copyright (C) 2011 Michael Babker. All rights reserved.
 * @license		GNU/GPL - http://www.gnu.org/copyleft/gpl.html
-* 
+*
 * Podcast Manager is based upon the ideas found in Podcast Suite created by Joe LeBlanc
 * Original copyright (c) 2005 - 2008 Joseph L. LeBlanc and released under the GPLv2 license
 */
+
+// Restricted access
+defined('_JEXEC') or die;
 
 /**
  * @package		Podcast Manager
@@ -17,7 +20,7 @@ abstract class PodcastMediaHelper
 {
 	/**
 	 * Gets the icon type
-	 * 
+	 *
 	 * @param string The filename
 	 * @return boolean
 	 */
