cd8bd2a28f36156761038e27b4234a408d839054
Michael Babker <mbabker@flbab.com>
2011-06-20 03:07:26 +0430

    Optimize media manager nav for 1.7, add language string

--- com_podcastmedia/admin/views/media/tmpl/default_navigation.php
+++ com_podcastmedia/admin/views/media/tmpl/default_navigation.php
@@ -4,7 +4,7 @@
 *
 * @copyright	Copyright (C) 2011 Michael Babker. All rights reserved.
 * @license		GNU/GPL - http://www.gnu.org/copyleft/gpl.html
-* 
+*
 * Podcast Manager is based upon the ideas found in Podcast Suite created by Joe LeBlanc
 * Original copyright (c) 2005 - 2008 Joseph L. LeBlanc and released under the GPLv2 license
 */
@@ -15,29 +15,17 @@ $app	= JFactory::getApplication();
 $style = $app->getUserStateFromRequest('podcastmedia.list.layout', 'layout', 'thumbs', 'word');
 ?>
 <div id="submenu-box">
-	<div class="t">
-		<div class="t">
-			<div class="t"></div>
+	<div class="submenu-box">
+		<div class="submenu-pad">
+			<ul id="submenu" class="media">
+				<li><a href="index.php?option=com_podcastmanager&amp;view=feeds"><?php echo JText::_('COM_PODCASTMEDIA_SUBMENU_FEEDS'); ?></a></li>
+				<li><a href="index.php?option=com_podcastmanager&amp;view=podcasts"><?php echo JText::_('COM_PODCASTMEDIA_SUBMENU_PODCASTS'); ?></a></li>
+				<li><a href="index.php?option=com_podcastmanager&amp;view=info"><?php echo JText::_('COM_PODCASTMEDIA_SUBMENU_INFO'); ?></a></li>
+				<li><a href="#" id="thumbs" onclick="PodcastMediaManager.setViewType('thumbs')" class="<?php echo ($style == "thumbs") ? 'active' : '';?>">
+				<?php echo JText::_('COM_PODCASTMEDIA_SUBMENU_FILES'); ?></a></li>
+			</ul>
+			<div class="clr"></div>
 		</div>
 	</div>
-	<div class="m">
-		<div class="submenu-box">
-			<div class="submenu-pad">
-				<ul id="submenu" class="media">
-					<li><a href="index.php?option=com_podcastmanager&amp;view=feeds"><?php echo JText::_('COM_PODCASTMEDIA_SUBMENU_FEEDS'); ?></a></li>
-					<li><a href="index.php?option=com_podcastmanager&amp;view=podcasts"><?php echo JText::_('COM_PODCASTMEDIA_SUBMENU_PODCASTS'); ?></a></li>
-					<li><a href="index.php?option=com_podcastmanager&amp;view=info"><?php echo JText::_('COM_PODCASTMEDIA_SUBMENU_INFO'); ?></a></li>
-					<li><a id="thumbs" onclick="PodcastMediaManager.setViewType('thumbs')" class="<?php echo ($style == "thumbs") ? 'active' : '';?>">
-					<?php echo JText::_('COM_PODCASTMEDIA_SUBMENU_FILES'); ?></a></li>
-				</ul>
-				<div class="clr"></div>
-			</div>
-		</div>
-		<div class="clr"></div>
-	</div>
-	<div class="b">
-		<div class="b">
-			<div class="b"></div>
-		</div>
-	</div>
+	<div class="clr"></div>
 </div>
