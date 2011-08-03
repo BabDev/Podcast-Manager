3c9701a2b6bb1a49cd24d851a2611ae5eb0d5e00
Michael Babker <mbabker@flbab.com>
2011-08-04 03:05:04 +0430

    Docblocks

--- plg_content_podcastmanager/soundmanager/script/player-uncompressed.js
+++ plg_content_podcastmanager/soundmanager/script/player-uncompressed.js
@@ -1,5 +1,16 @@
-/*
+/**
+* Podcast Manager for Joomla!
+*
+* @copyright	Copyright (C) 2011 Michael Babker. All rights reserved.
+* @license		GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+* @package		PodcastManager
+* @subpackage	plg_content_podcastmanager
+*
+* Podcast Manager is based upon the ideas found in Podcast Suite created by Joe LeBlanc
+* Original copyright (c) 2005 - 2008 Joseph L. LeBlanc and released under the GPLv2 license
+*/
 
+/*
   SoundManager 2 Demo: Play MP3 links "in-place"
   ----------------------------------------------
 
