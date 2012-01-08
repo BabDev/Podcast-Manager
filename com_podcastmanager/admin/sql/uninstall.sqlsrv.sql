628e7c21a08d7eca2857d02e859cdc0b30949d9b
Michael Babker <mbabker@flbab.com>
2012-01-08 14:23:14 +0100

    Fix uninstall

--- com_podcastmanager/admin/sql/uninstall.sqlsrv.sql
+++ com_podcastmanager/admin/sql/uninstall.sqlsrv.sql
@@ -1,7 +1,5 @@
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[#__podcastmanager]') AND type in (N'U'))
-DROP TABLE [dbo].[#__podcastmanager]
-GO
+DROP TABLE [dbo].[#__podcastmanager];
 
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[#__podcastmanager_feeds]') AND type in (N'U'))
-DROP TABLE [dbo].[#__podcastmanager_feeds]
-GO
+DROP TABLE [dbo].[#__podcastmanager_feeds];
