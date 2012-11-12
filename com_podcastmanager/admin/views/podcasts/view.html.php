dfb95570ebf30aa07fdf3d35b879cedbaa936406
Michael Babker <mbabker@flbab.com>
2012-11-11 23:11:36 -0600

    Batch button size

--- com_podcastmanager/admin/views/podcasts/view.html.php
+++ com_podcastmanager/admin/views/podcasts/view.html.php
@@ -168,7 +168,7 @@ class PodcastManagerViewPodcasts extends JViewLegacy
 		{
 			$bar = JToolBar::getInstance('toolbar');
 			$title = JText::_('JTOOLBAR_BATCH');
-			$dhtml = '<button data-toggle="modal" data-target="#collapseModal" class="btn">'
+			$dhtml = '<button data-toggle="modal" data-target="#collapseModal" class="btn btn-small">'
 					. '<i class="icon-checkbox-partial" title="' . $title . '"></i>'
 					. $title . '</button>';
 			$bar->appendButton('Custom', $dhtml, 'batch');
