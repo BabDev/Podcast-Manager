d7833c1b13a60bffeb1643facf92267e00f81221
Michael Babker <mbabker@flbab.com>
2013-01-06 16:11:09 -0600

    Copyright notices

--- mod_podcastmanager/mod_podcastmanager.php
+++ mod_podcastmanager/mod_podcastmanager.php
@@ -5,7 +5,7 @@
  * @package     PodcastManager
  * @subpackage  mod_podcastmanager
  *
- * @copyright   Copyright (C) 2011-2012 Michael Babker. All rights reserved.
+ * @copyright   Copyright (C) 2011-2013 Michael Babker. All rights reserved.
  * @license     GNU/GPL - http://www.gnu.org/copyleft/gpl.html
  *
  * Podcast Manager is based upon the ideas found in Podcast Suite created by Joe LeBlanc
