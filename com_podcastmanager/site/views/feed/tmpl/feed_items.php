112ffaa3c1c809f835a6dc9f0149af73c4d786d2
Michael Babker <mbabker@flbab.com>
2013-08-05 17:29:59 -0500

    Don't use deprecated API

--- com_podcastmanager/site/views/feed/tmpl/feed_items.php
+++ com_podcastmanager/site/views/feed/tmpl/feed_items.php
@@ -125,7 +125,7 @@ $listDirn	= $this->escape($this->state->get('list.direction'));
 		</tbody>
 	</table>
 
-	<?php if (($this->params->def('show_pagination', 1) == 1  || ($this->params->get('show_pagination') == 2)) && ($this->pagination->get('pages.total') > 1)) : ?>
+	<?php if (($this->params->def('show_pagination', 1) == 1  || ($this->params->get('show_pagination') == 2)) && ($this->pagination->pagesTotal > 1)) : ?>
 		<div class="pagination">
 			<?php if ($this->params->def('show_pagination_results', 1)) : ?>
 			<p class="counter pull-right">
